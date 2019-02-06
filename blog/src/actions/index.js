2702363caeb22b0ba7984c1a4be9f3d0047ff33b
smithwebtek <brad@smithwebtek.com>
2019-02-06 06:44:34 -0500

    Refactor index.js

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -7,21 +7,36 @@ export const fetchPostsAndUsers = () => async dispatch => {
 	console.log('fetched posts');
 }
 
-export const fetchPosts = () => async dispatch => {
-	const response = await jsonPlaceholder.get('/posts');
-	dispatch({ type: 'FETCH_POSTS', payload: response });
+export const fetchPosts = () => {
+	return async function (dispatch, getState) {
+		const response = await jsonPlaceholder.get('/posts');
+
+		dispatch({ type: 'FETCH_POSTS', payload: response });
+	};
 };
+// refactor
+// export const fetchPosts = () => async dispatch => {
+// 	const response = await jsonPlaceholder.get('/posts');
+// 	dispatch({ type: 'FETCH_POSTS', payload: response });
+// };
 
-export const fetchUser = (id) => async dispatch => {
-	const response = await jsonPlaceholder.get(`/users/${id}`);
-
-	dispatch({ type: 'FETCH_USER', payload: response.data });
+export const fetchUser = (id) => {
+	return async function (dispatch, getState) {
+		const response = await jsonPlaceholder.get(`/users/${id}`);
+		debugger;
+		dispatch({ type: 'FETCH_USER', payload: response.data });
+	};
 };
-
-
-
-// const _fetchUser = _.memoize(async (id, dispatch) => {
+// refactor
+// export const fetchUser = (id) => async dispatch => {
 // 	const response = await jsonPlaceholder.get(`/users/${id}`);
 
 // 	dispatch({ type: 'FETCH_USER', payload: response.data });
-// });
+// };
+
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+	const response = await jsonPlaceholder.get(`/users/${id}`);
+
+	dispatch({ type: 'FETCH_USER', payload: response.data });
+});
