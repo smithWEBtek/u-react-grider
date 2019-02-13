170994904e97a9ded0f1a4b3b1c7d66952bf86b7
smithwebtek <brad@smithwebtek.com>
2019-02-13 06:37:31 -0500

    Add action creators [streams app]

--- streams/client/src/reducers/authReducer.js
+++ streams/client/src/reducers/authReducer.js
@@ -1,8 +1,9 @@
-import { SIGN_IN, SIGN_OUT } from '../actions/types';
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from '../actions/types';
 
 const INITIAL_STATE = {
   isSignedIn: null,
-  userId: null
+  userId: null,
+  streams: []
 }
 
 export default (state = INITIAL_STATE, action) => {
@@ -11,6 +12,9 @@ export default (state = INITIAL_STATE, action) => {
       return { ...state, isSignedIn: true, userId: action.payload }
     case SIGN_OUT:
       return { ...state, isSignedIn: false, userId: null }
+    case CREATE_STREAM:
+      console.log('[CREATE_STREAM]updated state: ', { ...state, streams: [...state.streams, action.payload] });
+      return { ...state, streams: [...state.streams, action.payload] }
     default:
       return state;
   }
