0452857dff6f133642ac0bc673da6b734075d85f
smithwebtek <brad@smithwebtek.com>
2019-02-13 00:05:43 -0500

    Stub out redux form, validation

--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -4,8 +4,8 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 
 
-import App from './components/App';
 import reducers from './reducers';
+import App from './components/App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
