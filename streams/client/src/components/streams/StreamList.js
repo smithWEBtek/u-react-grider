c7a2dc86dfcb729256b429acc45afa0ee25800ac
smithwebtek <brad@smithwebtek.com>
2019-02-13 22:20:08 -0500

    Update streamslist

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -1,42 +1,66 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../actions';
 
 class StreamList extends React.Component {
-
-
   componentDidMount() {
     this.props.fetchStreams();
   }
 
-  render() {
-    const streams = this.props.streams
-    let renderList = '<div>Fetching streams ....</div>'
-    renderList = streams.map((stream) => {
+  renderAdmin(stream) {
+    if (this.props.currentUserId === stream.userId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      )
+    }
+  }
+
+  renderList() {
+    return this.props.streams.map((stream) => {
       return (
         <div key={stream.id} className="item">
+          {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
             <div className="description">{stream.description}</div>
           </div>
         </div>
-      )
-    })
+      );
+    });
+  }
 
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: 'right' }}>
+          <Link to="/streams/new" className="ui button positive">Create Stream</Link>
+        </div>
+      )
+    }
+  };
+
+  render() {
     return (
       <div>
         <h2>Streams</h2>
-        <div className="ui celled list">{renderList}</div>
-      </div>
-    )
+        <div className="ui celled list">{this.renderList()}</div>
+        {this.renderCreate()}
+      </div >
+    );
   }
-};
+}
 
 const mapStateToProps = state => {
   return {
-    streams: Object.values(state.streams)
-  }
-}
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
+  };
+};
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
--- streams/client/src/reducers/authReducer.js
+++ streams/client/src/reducers/authReducer.js
@@ -1,4 +1,4 @@
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from '../actions/types';
+import { SIGN_IN, SIGN_OUT } from '../actions/types';
 
 const INITIAL_STATE = {
   isSignedIn: null,
@@ -11,9 +11,7 @@ export default (state = INITIAL_STATE, action) => {
       return { ...state, isSignedIn: true, userId: action.payload }
     case SIGN_OUT:
       return { ...state, isSignedIn: false, userId: null }
-    case CREATE_STREAM:
-      console.log('[CREATE_STREAM]updated state: ', { ...state, streams: [...state.streams, action.payload] });
-      return { ...state, streams: [...state.streams, action.payload] }
+
     default:
       return state;
   }
